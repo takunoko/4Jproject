616d6df3d7b0cb1243f5c98037a31995414aa2a2
takunoko <takunoko@gmail.com>
2014-12-17 03:12:43 +0900

    network にMakefile追加 main.cで簡易的な表示関数を実装

--- main.c
+++ main.c
@@ -7,7 +7,12 @@
 // 手札の最大枚数
 #define HAND_CARD_SIZE 25
 
+#define NEW_GAME_YES 1
+#define NEW_GAME_NO 2
+
+// 各プレイヤーの状態を保持する
 typedef struct {
+	int id;
 	int card[HAND_CARD_SIZE]; // プレイヤーの持っているカード Maxで25枚ぐらいしか持たないでしょ
 	int card_num;	// カードの枚数を保持
 	int play_flg; // このユーザーがゲームに勝利とかしていないかの判定
@@ -16,8 +21,14 @@ typedef struct {
 void init_program();
 int set_player();
 void init_game( int, PLAYER*);
-void change_int( int*, int*);
+
 void game();
+int ask_new_game();
+void change_int( int*, int*);
+
+void player_do(int, PLAYER*, int);
+void select_card(int do_player_num, PLAYER* players, int player_size);
+int chk_select_card( PLAYER, int);
 
 int main(void){
 	init_program();	// なんか、乱数の初期化とかするときに使う
@@ -32,6 +43,7 @@ void init_program(){
 }
 
 // 人数の決定
+// このへんで、ネットワークで人数をうまく決定したい
 int set_player(){
 	int player_num = 0;
 	do{
@@ -82,6 +94,7 @@ void init_game(int player_size, PLAYER players[]){
 	// 配られたカードの確認
 	for(j=0; j<player_size; j++){
 		printf("P:%d  ", j);
+		players[j].id = j;
 		for(i=0; i<players[j].card_num; i++){
 			printf(" %2d,", players[j].card[i]);
 			// 表示内容
@@ -95,18 +108,76 @@ void init_game(int player_size, PLAYER players[]){
 // ゲーム開始
 void game(){
 	int player_size;
+	int new_game;
 	PLAYER players[6];
 
 	player_size = set_player();
 
 	// ゲームループ
-	while(1){
+	do{
+		int turn_cnt;
+		// ゲームの初期化
 		init_game( player_size, players);
 
-		// このへんにゲームの更新処理を書いていく
-		//
-		break;
+		// ひとり1行動
+		while(1){
+			player_do( turn_cnt % player_size, players, player_size);
+			turn_cnt++;
+		}
+		new_game = ask_new_game();
+	}while(new_game == NEW_GAME_YES);
+}
+
+// 新しいゲームを始めるかの質問
+int ask_new_game(){
+	char input;
+	int ret;
+	puts("新しいゲームを始めますか?");
+	printf("[ Yes:y, No:n] ");
+	scanf(" %c", &input);
+
+	switch(input){
+		case 'y':
+		case 'Y':
+			ret = NEW_GAME_YES;
+			break;
+		default :
+			ret = NEW_GAME_NO;
+			break;
 	}
+
+	return ret;
+}
+
+// player_do( 自分のプレイヤー番号, 全員の手札構造体, 全体のプレイヤー数)
+void player_do(int do_player_num, PLAYER* players, int player_size){
+	select_card( do_player_num, players, player_size);
+}
+
+// むっちゃ適当に手札表示関数を作ってみた
+void select_card(int do_player_num, PLAYER* players, int player_size){
+	int i,j;
+	int choice_card;
+
+	// とりあえず、全員分適当に表示
+	do{
+		for (i = (do_player_num+1)%player_size; i != do_player_num%player_size; i = (i+1)%player_size) {
+			printf("Player : %d\n", i);
+			for (j = 0; j < players[i].card_num; j++) {
+				printf(" %2d",players[i].card[j]);
+			}
+			putchar('\n');
+		}
+		scanf("%d", &choice_card);
+		// この辺で"選択できません"とか、コメント出すべきな気がする
+	}while(chk_select_card( players[do_player_num%player_size], choice_card));
+}
+
+// 可:0 不可:1
+int chk_select_card(PLAYER my_card, int choice_card){
+	// 手札と選択したカードから出せるか判断
+	// テストとして、とりあえず1を表示しておく
+	return 0;
 }
 
 // 交換
@@ -116,5 +187,3 @@ void change_int( int *x, int *y){
 	*x = *y;
 	*y = tmp;
 }
-
-
